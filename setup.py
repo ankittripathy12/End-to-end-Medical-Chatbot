3e49befcde23ffc38b984c3704a9965f389c3304
ankittripathy12 <ankittriapthy372@gmail.com>
2026-07-24 13:11:17 +0530

    Requirements installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "medical-chatbot",
+    version= "0.1.0",
+    author= "Ankit Kumar Tripathy",
+    author_email="ankitatwork321@gmail.com",
+    packages= find_packages(),
+    intstall_requires=[]
+)
